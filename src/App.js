a8dff2fb692b992f0c5f5407c5667a12a0821866
sarveshatale-stack <71748819+sarveshatale-stack@users.noreply.github.com>
2020-09-28 20:22:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import HelloWorld from "./components/HelloWorld";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hello World</h1> */}
+      <HelloWorld />
+    </div>
+  );
+}
