a3e8bcd43e4ec32499d566d69838c25f55e5e580
sarveshatale-stack <71748819+sarveshatale-stack@users.noreply.github.com>
2020-10-03 22:10:41 +0530

    Test Case using dom

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,15 @@
+import React from "react";
 import App from "./App";
+import HelloWorld from "./components/HelloWorld";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import ReactDOM from "react-dom";
 configure({ adapter: new Adapter() });
+it("renders App", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<HelloWorld />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 describe("My Test Suite", () => {
   it("My Test Case", () => {
     expect(true).toEqual(true);
